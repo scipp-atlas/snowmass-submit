f15206f44d423d573952cbfbbc113a0da186edcf
Giordon Stark <kratsg@gmail.com>
2021-11-22 10:30:28 -0600

    update process.sh

--- process.sh
+++ process.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-export HOME=$(pwd)
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
-
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
+
+printenv
 
 for ((i=1; i<=$#; i++))
 do
   printf "${i}: ${!i}\n"
 done
 
-touch ${2}
+python3 Delphes2SA.py --input ${1} --output ${2}
