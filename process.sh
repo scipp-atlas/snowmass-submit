363142a1968d2cd2af0f30c0295b30ffc5247930
Giordon Stark <kratsg@gmail.com>
2021-11-22 02:47:28 -0600

    first working scripts

---
+++ process.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export HOME=$(pwd)
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh --quiet
+
+printf "Start time: "; /bin/date
+printf "Job is running on node: "; /bin/hostname
+printf "Job running as user: "; /usr/bin/id
+printf "Job is running in directory: "; /bin/pwd
+
+for ((i=1; i<=$#; i++))
+do
+  printf "${i}: ${!i}\n"
+done
+
+touch ${2}
